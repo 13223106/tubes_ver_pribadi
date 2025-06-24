9a3b6a4154acade26bdc4dbcda2c386b096197cd
13223106@std.stei.itb.ac.id <13223106@std.stei.itb.ac.id>
2025-06-24 10:54:06 +0700

    Delete tubes_pmp-main directory

--- tubes_pmp-main/src/main_test.c
+++
@@ -1,9 +0,0 @@
-#include "penjadwalan.h"
-#include "showSchedule.h"
-#include "data.h"
-int main() {
-    //Jadwal* jadwal = buat_jadwal_hari(dokter);  // ← di penjadwalan.h
- 
-    //tampilkan_jadwal(jadwal);
-    return 0;
-}
--- tubes_pmp-main/src/memory_handling.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEMORY_HANDLING
-#define MEMORY_HANDLING
-#include "data.h"
-void tambah_dokter(Dokter** data_dokter, int* size);
-void hapus_dokter(Dokter** data_dokter, int* size);
-void ubah_dokter(Dokter* data_dokter);
-
-#endif
